f3fac752caa1d2f3f396af9c11061a5431763d50
Alisher <alish.oroz@mail.ru>
2024-05-03 00:18:32 +0600

    бутур

--- sabak_15_bmi_colculator/lib/features/presentation/widgets/caltulate_button.dart
+++ sabak_15_bmi_colculator/lib/features/presentation/widgets/caltulate_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sabak_15_bmi_colculator/features/presentation/constants/app_text_styles.dart';
 
 class CalculateButton extends StatelessWidget {
@@ -13,55 +14,100 @@ class CalculateButton extends StatelessWidget {
             builder: (BuildContext context) {
               return Card(
                 margin:
-                     const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
-                color:  const Color(0xff0b0120),
-                child: Column(
-                  children: [
-                    const Row(
-                      children: [
-                        Icon(Icons.arrow_back,
-                        color: Colors.white,
-                        size: 32,
-                       ),
-                        
-                        Text('Ден-соолук индекси(BMI)',
-                            style: AppTextStyles.heightStyle),
-                      ],
-                    ),
-                    const Text('Жыйынтык', style: AppTextStyles.heightStyle),
-                    Column(
-                      children: [
-                        const Text(
-                          'Нормалдуу',
-                          style: TextStyle(
-                              color: Color(0xff08E82C),
-                              fontSize: 24,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        const Text(
-                          '24.2',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 80,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        const Text(
-                          'Сиздин дене салмагыныз нормалдуу. Азаматсыз!',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Кайра эсепте',
-                            style: AppTextStyles.bodyStyle,
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+                color: const Color(0xff0b0120),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      const Row(
+                        children: [
+                          Icon(
+                            Icons.arrow_back_ios,
+                            color: Colors.white,
+                            size: 32,
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text('Ден-соолук индекси(BMI)',
+                              style: AppTextStyles.heightStyle),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Жыйынтык',
+                              style: AppTextStyles.heightStyle)),
+                      Column(
+                        children: [
+                          const SizedBox(
+                            height: 58,
+                          ),
+                          Container(
+                            width: 500,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              border: Border.all(color: Colors.white),
+                            ),
+                            child: const Column(
+                              children: [
+                                Text(
+                                  'Нормалдуу',
+                                  style: TextStyle(
+                                      color: Color(0xff08E82C),
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 36,
+                                ),
+                                Text(
+                                  '24.2',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 80,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 24,
+                                ),
+                                Text(
+                                  'Сиздин дене салмагыныз нормалдуу. Азаматсыз!',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 3.4,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.deepOrange,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                minimumSize: Size(
+                                    MediaQuery.of(context).size.width, 38)),
+                            onPressed: () {},
+                            child: const Text(
+                              'Кайра эсепте',
+                              style: AppTextStyles.bodyStyle,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             });
