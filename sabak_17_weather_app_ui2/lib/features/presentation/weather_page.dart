cdad63d91a09d641951ebfa796bc1bc2623acbe8
Alisher <alish.oroz@mail.ru>
2024-05-20 23:36:58 +0600

    Done

--- sabak_17_weather_app_ui2/lib/features/presentation/weather_page.dart
+++ sabak_17_weather_app_ui2/lib/features/presentation/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:sabak_17_weather_app_ui2/features/presentation/widgets/card_widget.dart';
 import 'package:sabak_17_weather_app_ui2/features/presentation/widgets/city_name.dart';
 import 'package:sabak_17_weather_app_ui2/features/presentation/widgets/search_setting.dart';
@@ -20,48 +21,79 @@ class _WeaterPageState extends State<WeatherPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              const SearchSettingWidget(),
-              const CityNameWidget(),
-              const TemperatureViewWidget(),
-              const CardWidget(),
-              const CardWidget(),
-              const CardWidget(),
-              const WheatherDaysWidget(),
-              SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                activeTickMarkColor:Colors.white,
-                inactiveTickMarkColor: Colors.grey,
-                thumbColor: Colors.red,
-
+        body: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 27),
+              child: Column(
+                children: [
+                  const SearchSettingWidget(),
+                  const CityNameWidget(),
+                  const TemperatureViewWidget(),
+                  const CardWidget(),
+                  const CardWidget(),
+                  const CardWidget(),
+                  const WheatherDaysWidget(),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTickMarkColor: Colors.white,
+                      inactiveTickMarkColor: Colors.grey,
+                      thumbColor: Colors.red,
+                    ),
+                    child: Slider(
+                      mouseCursor: MouseCursor.defer,
+                      divisions: 2,
+                      label: scroll.round().toString(),
+                      min: 0,
+                      max: 3,
+                      value: scroll,
+                      onChanged: (value) {
+                        setState(() {
+                          scroll = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
-              child: Slider(
-                mouseCursor: MouseCursor.defer,
-                divisions: 2,
-                label: scroll.round().toString(),
-                min: 0,
-                max: 3,
-                value: scroll,
-                onChanged: (value) {
-                  setState(() {
-                    scroll = value;
-                    print("value maanisi");
-                    print("scroll maanisi");
-                  }
+            ),
+            SizedBox(
+              height: 15.52,
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.62),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(34.8),
+                      child: Container(
+                        width: 55.15,
+                        height: 88,
+                        color: Colors.white.withOpacity(0.3),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('data'),
+                            SvgPicture.asset('assets/svg_images/Ellipse 14.svg'),
+                            Text('data')
+                          ],
+                        ),
+                      ),
+                    ),
                   );
                 },
-              ) ,
               ),
-             
-              
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
   }
 }
-
